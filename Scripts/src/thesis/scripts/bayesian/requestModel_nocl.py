6c2368ded7585327c9503a50f9837b189938a9c2
Claudio <claudio@claudio-Aspire-M1610.(none)>
2012-05-29 00:25:42 +0200

    mcmc model for no cluterization

---
+++ Scripts/src/thesis/scripts/bayesian/requestModel_nocl.py
@@ -0,0 +1,33 @@
+'''
+Created on May 28, 2012
+
+@author: claudio
+'''
+
+'''
+Created on Aug 28, 2011
+
+@author: work
+'''
+from pymc import *
+#from thesis.scripts.examples.main_ex import main_mcmc
+from thesis.scripts.examples.article_main import initialize_wmproxy
+from numpy import matrix, int32
+import numpy
+
+#target of the Barnes-Hut
+#target = main_mcmc()
+
+ #initialization of data wmproxy
+traininput, traintarget, testinput, testtarget = initialize_wmproxy()
+
+
+
+rates = []
+requests = []
+for j in range(len(traintarget)):
+    #prior
+    rates.append(Normal('rate_%d' % (j), mu = traintarget[j], tau = 1.0, value = 1))
+    
+    #likelihood
+    requests.append(Poisson('request_%d' % (j), mu=rates[j], value=traintarget[j], observed=True))
